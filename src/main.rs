eae325465f0f11f998e59c5f86b98d93b74ae7b9
Romain B <rom.bou@hotmail.fr>
2024-02-14 19:34:43 +0100

    Move from bevy_entitiles to bevy_ecs_ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,9 @@
-use bevy::{
-    app::{App, PluginGroup, PostStartup, Update},
-    asset::{AssetServer, Assets},
-    core_pipeline::core_2d::Camera2dBundle,
-    ecs::{
-        component::Component,
-        event::EventReader,
-        query::With,
-        system::{Commands, EntityCommands, Query, Res, ResMut},
-    },
-    input::{keyboard::KeyCode, Input},
-    math::Vec2,
-    reflect::Reflect,
-    render::{mesh::Mesh, render_resource::FilterMode, texture::ImagePlugin, view::Msaa},
-    sprite::TextureAtlas,
-    utils::HashMap,
-    DefaultPlugins,
-};
-use bevy_entitiles::{
-    ldtk::{
-        app_ext::LdtkApp,
-        events::LdtkEvent,
-        json::{field::FieldInstance, level::EntityInstance},
-        layer::physics::LdtkPhysicsLayer,
-        resources::{LdtkAdditionalLayers, LdtkAssets, LdtkLevelManager, LdtkLoadConfig},
-        sprite::LdtkEntityMaterial,
-    },
-    tilemap::physics::PhysicsTile,
-    EntiTilesPlugin,
-};
-use bevy_entitiles_derive::{LdtkEntity, LdtkEntityTag};
-use bevy_xpbd_2d::{
-    components::{
-        Collider, Friction, LinearVelocity, LockedAxes, Mass, Position, RigidBody, Rotation,
-    },
-    math::{Scalar, Vector},
-    plugins::{
-        collision::Collisions, debug::PhysicsDebugConfig, PhysicsDebugPlugin, PhysicsPlugins,
-    },
-    resources::Gravity,
-};
-use character_controller::{CharacterControllerBundle, CharacterControllerPlugin};
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_ecs_ldtk::LdtkWorldBundle;
+use bevy_xpbd_2d::{math::Vector, prelude::*};
+
+use character_controller::CharacterControllerPlugin;
 use helpers::EntiTilesHelpersPlugin;
 
 mod character_controller;
@@ -49,110 +13,83 @@ fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(ImagePlugin::default_nearest()),
-            EntiTilesPlugin,
+            LdtkPlugin,
             EntiTilesHelpersPlugin::default(),
-            PhysicsPlugins::default(),
-            PhysicsDebugPlugin::default(),
-            CharacterControllerPlugin,
         ))
-        .add_systems(PostStartup, setup)
-        .add_systems(Update, (events))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (despawn_collision_tile, despawn_door_tile))
         .insert_resource(Msaa::Off)
-        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
-        .insert_resource(PhysicsDebugConfig::all())
-        .register_type::<Player>()
-        .insert_resource(LdtkLoadConfig {
-            file_path: "assets/map.ldtk".to_string(),
-            filter_mode: FilterMode::Nearest,
-            ignore_unregistered_entities: false,
+        .insert_resource(LevelSelection::index(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
             ..Default::default()
         })
-        .insert_resource(LdtkAdditionalLayers {
-            physics_layer: Some(LdtkPhysicsLayer {
-                identifier: "PhysicsColliders".to_string(),
-                air: 0,
-                parent: "Collisions".to_string(),
-                tiles: Some(HashMap::from([(
-                    1,
-                    PhysicsTile {
-                        rigid_body: true,
-                        friction: Some(0.9),
-                    },
-                )])),
-            }),
-            ..Default::default()
-        })
-        .register_ldtk_entity::<Player>("Player")
-        .register_ldtk_entity_tag::<Actor>("actor")
+        .register_ldtk_int_cell_for_layer::<CollisionTileBundle>("Collisions", 1)
+        .register_ldtk_int_cell_for_layer::<DoorTileBundle>("Collisions", 2)
+        .register_ldtk_entity::<PlayerBundle>("Player")
         .run();
 }
 
-fn setup(mut commands: Commands, mut manager: ResMut<LdtkLevelManager>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
     commands.spawn(Camera2dBundle::default());
 
-    // Load Level
-    manager.load(&mut commands, "Level_0".to_string(), None);
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("map.ldtk"),
+        ..Default::default()
+    });
 }
 
-fn events(mut ldtk_events: EventReader<LdtkEvent>) {
-    for event in ldtk_events.read() {
-        match event {
-            LdtkEvent::LevelLoaded(level) => {
-                println!("Level loaded: {}", level.identifier);
-            }
-            LdtkEvent::LevelUnloaded(level) => {
-                println!("Level unloaded: {}", level.identifier);
-            }
-        }
+#[derive(Default, Component)]
+struct CollisionTile;
+
+#[derive(Default, Bundle, LdtkIntCell)]
+struct CollisionTileBundle {
+    collision_tile: CollisionTile,
+}
+
+#[derive(Default, Component)]
+struct DoorTile;
+
+#[derive(Default, Bundle, LdtkIntCell)]
+struct DoorTileBundle {
+    doorr_tile: DoorTile,
+}
+
+fn despawn_collision_tile(
+    mut commands: Commands,
+    collision_tile_query: Query<Entity, Added<CollisionTile>>,
+) {
+    for entity in &collision_tile_query {
+        commands.entity(entity).despawn_recursive();
     }
 }
 
-// this function will be called when the player entity is GOING TO BE spawned
-// which means the entity still has no components
-// you can consider this as a "extension"
-// you don't need to impl the entire LdtkEntity trait but still able to do something
-// that are not supported by generated code
-fn player_spawn(
-    // the entity commands for the entity
-    commands: &mut EntityCommands,
-    // all the data from ldtk
-    entity_instance: &EntityInstance,
-    // the fields of this entity you can access them using their identifiers(ldtk side)
-    // generally you don't need to use this, and this fields will be applyed to the entity later
-    // with generated code
-    _fields: &HashMap<String, FieldInstance>,
-    // the asset server
-    _asset_server: &AssetServer,
-    // the ldtk assets, like sprites and meshes
-    _ldtk_assets: &LdtkAssets,
+fn despawn_door_tile(mut commands: Commands, door_tile_query: Query<Entity, Added<DoorTile>>) {
+    for entity in &door_tile_query {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+#[derive(Default, Component)]
+struct Player;
+
+#[derive(Default, Bundle, LdtkEntity)]
+struct PlayerBundle {
+    player: Player,
+    #[sprite_sheet_bundle]
+    sprite_sheet_bundle: SpriteSheetBundle,
+    #[worldly]
+    worldly: Worldly,
+}
+
+fn player_initial_position(
+    mut player_query: Query<(&mut Transform, &EntityInstance), Added<Player>>,
 ) {
-    // this is takes params that are exactly the same as the LdtkEntity trait
-    // you can use this to add more fancy stuff to your entity
-    // like adding a collider:
-    let width = entity_instance.width as f32;
-    let height = entity_instance.height as f32;
-    let cuboid = Collider::cuboid(width, height);
+    let (mut player_transform, entity_instance) = player_query.single_mut();
 
-    commands.insert((
-        // RigidBody::Kinematic,
-        // LockedAxes::ROTATION_LOCKED,
-        CharacterControllerBundle::new(cuboid).with_movement(1250.0, 0.92),
-    ));
+    player_transform.translation.x = entity_instance.world_x.unwrap() as f32;
+    player_transform.translation.y = entity_instance.world_y.unwrap() as f32;
 }
-
-#[derive(Component, Default, LdtkEntity, Reflect)]
-#[spawn_sprite]
-// this means the entity will not disappear when the level is unloaded
-#[global_entity]
-#[callback(player_spawn)]
-pub struct Player {
-    #[ldtk_name = "HP"]
-    pub hp: i32,
-    // this will be deafult as it not exists in the ldtk file
-    #[ldtk_default]
-    pub mp: i32,
-}
-
-#[derive(Component, LdtkEntityTag)]
-pub struct Actor;

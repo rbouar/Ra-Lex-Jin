2682476411225df1e6e523f46c88f1244f53a38b
Romain B <rom.bou@hotmail.fr>
2024-02-15 22:13:31 +0100

    Add automatic collisions creation

--- src/dungeon/mod.rs
+++ src/dungeon/mod.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+use collisions::spawn_wall_collision;
+
+mod collisions;
 pub struct DungeonPlugin;
 
 impl Plugin for DungeonPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_ldtk)
-            .add_systems(Update, (despawn_collision_tile, despawn_door_tile))
+            .add_systems(Update, (spawn_wall_collision, hide_collisions_layer))
             .insert_resource(LevelSelection::index(0))
             .insert_resource(LdtkSettings {
                 set_clear_color: SetClearColor::FromLevelBackground,
@@ -15,10 +18,12 @@ impl Plugin for DungeonPlugin {
                 },
                 ..Default::default()
             })
-            .register_ldtk_int_cell_for_layer::<CollisionTileBundle>("Collisions", 1)
-            .register_ldtk_int_cell_for_layer::<DoorTileBundle>("Collisions", 2);
+            .register_ldtk_int_cell_for_layer::<CollisionTileBundle>(COLLISIONS_LAYER_ID, 1)
+            .register_ldtk_int_cell_for_layer::<DoorTileBundle>(COLLISIONS_LAYER_ID, 2);
     }
 }
+
+const COLLISIONS_LAYER_ID: &str = "Collision";
 
 #[derive(Default, Component)]
 pub struct CollisionTile;
@@ -36,18 +41,11 @@ pub struct DoorTileBundle {
     pub door_tile: DoorTile,
 }
 
-fn despawn_collision_tile(
-    mut commands: Commands,
-    collision_tile_query: Query<Entity, Added<CollisionTile>>,
-) {
-    for entity in &collision_tile_query {
-        commands.entity(entity).despawn_recursive();
-    }
-}
-
-fn despawn_door_tile(mut commands: Commands, door_tile_query: Query<Entity, Added<DoorTile>>) {
-    for entity in &door_tile_query {
-        commands.entity(entity).despawn_recursive();
+fn hide_collisions_layer(mut layer_query: Query<(&mut Visibility, &LayerMetadata), Added<LayerMetadata>>) {
+    for (mut visibility, layer_metadata) in layer_query.iter_mut() {
+        if layer_metadata.identifier == COLLISIONS_LAYER_ID {
+            *visibility = Visibility::Hidden;
+        }
     }
 }
 
